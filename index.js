979cff8849bde3f4441472167d8c80f04ae23bdf
Ashraf shawwa <ashraf_shawa@hotmail.com>
2022-12-09 17:01:43 +0200

    POST using fetch

--- index.js
+++ index.js
@@ -10,16 +10,57 @@ addBtn.onclick = _ => {
     addBtn.style.display = "none"
     formBtns.style.display = "flex"
     inputForm.style.display = "block"
-    scrollTo(0, 23880)
+    scrollTo(0, document.body.scrollHeight);
 }
 
 closeBtn.onclick = _ => {
     addBtn.style.display = "block"
     formBtns.style.display = "none"
     inputForm.style.display = "none"
-
 }
 
+createBtn.onclick = _ => {
+    addBtn.style.display = "block"
+    formBtns.style.display = "none"
+    inputForm.style.display = "none"
+    createToDo()
+}
+
+
+function createToDo() {
+
+    const myPost = {
+        userId: parseInt(Math.random() * 10),
+        title: document.getElementById("Title-Input").value,
+        body: document.getElementById("Description-Input").value,
+    };
+
+
+    // Post using Fetch 
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(myPost),
+    })
+        .then((res) => res.json())
+        .then((data) => console.log(data))
+        .catch((error) => {
+            console.log('error', error);
+        });
+
+    
+    let newToDo = `
+                        <div class="card">
+                            <div class="title">${myPost.title}</div>
+                            <div class="description">${myPost.body}</div>
+                        </div>
+                        
+                        `
+    document.getElementById("output").innerHTML += newToDo;
+
+}
 
 
 
@@ -47,12 +88,8 @@ function getToDos() {
             throw Error('sth went wrong');
         }
     };
-    
+
     xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
     xhr.send();
 };
 getToDos()
-
-
-
-
