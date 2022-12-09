54f952fdc9ebc1c015079589f3e54518081edff6
Ashraf shawwa <ashraf_shawa@hotmail.com>
2022-12-09 15:08:46 +0200

    last update for the AJAX , next step is the POST using fetch

--- index.js
+++ index.js
@@ -1,28 +1,5 @@
 
 function getToDos() {
-    // using fetch
-    
-    // fetch('https://jsonplaceholder.typicode.com/posts')
-    //     .then((res) => res.json())
-    //     .then((data) => {
-    //         let output ="";
-    //        data.forEach(element => {
-    //            output +=
-    //                `
-    //                 <div class="card">
-    //                     <div class="title">${element.title}</div>
-    //                     <div class="description">${element.body}</div>
-    //                 </div>
-
-    //                 `
-    //        });
-    //         document.getElementById("output").innerHTML = output;
-    //     })
-    //     .catch((error) => {
-    //         console.log('error', error);
-    //     });
-
-
     // using AJAX 
     const xhr = new XMLHttpRequest();
 
