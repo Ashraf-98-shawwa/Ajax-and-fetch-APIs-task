ff03b8a10055223278f9fa5c6d4ee5016e33c738
Ashraf shawwa <ashraf_shawa@hotmail.com>
2022-12-09 14:46:02 +0200

    READ Using AJAX and displaying the records in the document

---
+++ index.js
@@ -0,0 +1,57 @@
+
+function getToDos() {
+    // using fetch
+    
+    // fetch('https://jsonplaceholder.typicode.com/posts')
+    //     .then((res) => res.json())
+    //     .then((data) => {
+    //         let output ="";
+    //        data.forEach(element => {
+    //            output +=
+    //                `
+    //                 <div class="card">
+    //                     <div class="title">${element.title}</div>
+    //                     <div class="description">${element.body}</div>
+    //                 </div>
+
+    //                 `
+    //        });
+    //         document.getElementById("output").innerHTML = output;
+    //     })
+    //     .catch((error) => {
+    //         console.log('error', error);
+    //     });
+
+
+    // using AJAX 
+    const xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            let data = JSON.parse(xhr.response);
+            let output = "";
+            data.forEach(element => {
+                output +=
+                    `
+                        <div class="card">
+                            <div class="title">${element.title}</div>
+                            <div class="description">${element.body}</div>
+                        </div>
+                        
+                        `
+            });
+            document.getElementById("output").innerHTML = output;
+
+        } else if (xhr.readyState === 4 && xhr.status !== 200) {
+            throw Error('sth went wrong');
+        }
+    };
+
+    xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
+    xhr.send();
+};
+getToDos()
+
+
+
+
