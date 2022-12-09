5ff5aa4efccaa9eafaa5a48880c58fdcde0da2c5
Ashraf shawwa <ashraf_shawa@hotmail.com>
2022-12-09 16:15:16 +0200

    coding the html css nd basic js for the form and its buttons

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
 
+const addBtn = document.getElementById("add")
+const closeBtn = document.getElementById("close")
+const createBtn = document.getElementById("create")
+
+const formBtns = document.querySelector(".formBtns")
+const inputForm = document.querySelector(".input")
+
+addBtn.onclick = _ => {
+    addBtn.style.display = "none"
+    formBtns.style.display = "flex"
+    inputForm.style.display = "block"
+    scrollTo(0, 23880)
+}
+
+closeBtn.onclick = _ => {
+    addBtn.style.display = "block"
+    formBtns.style.display = "none"
+    inputForm.style.display = "none"
+
+}
+
+
+
+
 function getToDos() {
     // using AJAX 
     const xhr = new XMLHttpRequest();
